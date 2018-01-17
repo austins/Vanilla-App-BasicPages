5a1201f32ad3844a74291d7aed281f83e213500d
austins <austins@users.noreply.github.com>
2018-01-16 18:22:42 -0800

    Update deletepage view

--- views/pagessettings/deletepage.php
+++ views/pagessettings/deletepage.php
@@ -8,8 +8,7 @@
 echo $this->Form->Open();
 echo $this->Form->Errors();
 ?>
-    <div class="Buttons<?php if ($this->DeliveryType() == DELIVERY_TYPE_VIEW)
-        echo ' Footer'; // Popup ?>">
+    <div class="form-footer">
         <?php
         echo $this->Form->Button(T('BasicPages.Settings.DeletePage.OK', 'OK'), array('class' => 'Button Primary'));
         echo Anchor(T('BasicPages.Settings.Cancel', 'Cancel'), 'pagessettings/allpages', 'Button');
